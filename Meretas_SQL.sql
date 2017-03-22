397f41cd0711c1c72f253a03746c1af074bb8481
Jane Gallego <jgallego1@studentmail.nait.ca>
2017-03-22 16:55:21 -0600

    Reserve Tee Time Finished!
    - Reserve Tee Time Finished.

--- Meretas_SQL.sql
+++ Meretas_SQL.sql
@@ -1,20 +1,12 @@
 USE lachica1_Meretas
 
 
-CREATE TABLE Visitors
-(
-	VisitorID INT NOT NULL,
-	SessionID NVARCHAR(120) NOT NULL,
-	CONSTRAINT PK_VisitorID PRIMARY KEY (VisitorID)
-)
-
-GO
 CREATE TABLE Members 
 (
 	MemberID INT NOT NULL, 
 	MemberEmail NVARCHAR(120) NOT NULL,
 	MemberPassword NVARCHAR(120) NOT NULL,
-	MemberRole NVARCHAR(60) NOT NULL,
+	IsAdmin BIT NOT NULL
 	CONSTRAINT PK_MemberID PRIMARY KEY (MemberID)
 )
 GO
@@ -25,16 +17,27 @@ CREATE TABLE Surveys
 	CONSTRAINT PK_SurveyID PRIMARY KEY (SurveyID)
 )
 GO
-CREATE TABLE SurveyInstance
+CREATE TABLE SurveyInstance 
 (
-	VisitorID INT NOT NULL,
 	SurveyID INT NOT NULL,
+	MemberID INT NULL,
 	DateSubmitted DATE NOT NULL,
 	TimeSubmitted TIME NOT NULL, 
-	CONSTRAINT PK_SurveyInstance PRIMARY KEY (VisitorID, SurveyID),
-	CONSTRAINT FK_VisitorID FOREIGN KEY (VisitorID) REFERENCES Visitors(VisitorID),
-	CONSTRAINT FK_SurveyID FOREIGN KEY (SurveyID) REFERENCES Surveys(SurveyID)
+	CONSTRAINT PK_SurveyInstance PRIMARY KEY (SurveyID),
+	CONSTRAINT FK_SurveyInstanceSurveys FOREIGN KEY (SurveyID) REFERENCES Surveys(SurveyID),
+	CONSTRAINT FK_SurveyInstanceMembers FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
 )
 GO
-
-
+CREATE TABLE Questions
+(
+	SurveyID INT NOT NULL,
+	QuestionID INT NOT NULL,
+	QuestionText NVARCHAR(100) NOT NULL,
+	CONSTRAINT PK_Questions PRIMARY KEY (SurveyID, QuestionID),
+	CONSTRAINT FK_QuestionsSurveys FOREIGN KEY (SurveyID) REFERENCES Surveys(SurveyID),
+)
+GO
+CREATE TABLE 
+(
+)
+GO
