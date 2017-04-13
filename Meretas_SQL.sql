5d6df932f197d7bb0a1968b2d49f5e87c5b770f0
Jane Gallego <jgallego1@studentmail.nait.ca>
2017-04-12 19:49:42 -0600

    Added procedures for card filtering

--- Meretas_SQL.sql
+++ Meretas_SQL.sql
@@ -47,15 +47,24 @@ CREATE TABLE Choices
 	CONSTRAINT FK_ChoicesQuestions FOREIGN KEY (QuestionID, SurveyID) REFERENCES Questions(QuestionID, SurveyID)
 )
 GO
-CREATE TABLE QuestionResponse
+CREATE TABLE QuestionResponse 
 (	
 	QuestionID INT NOT NULL,
 	SurveyID INT NOT NULL,
 	SurveyResponseID INT NOT NULL,
-	ResponseText NVARCHAR(1),
-	CONSTRAINT PK_QuestionResponse PRIMARY KEY (QuestionID, SurveyID, SurveyResponseID),
-	CONSTRAINT FK_QuestionResponseQuestions FOREIGN KEY (QuestionID, SurveyID) REFERENCES Questions(QuestionID, SurveyID),
+	ChoiceID INT NOT NULL,
+	CONSTRAINT PK_QuestionResponse PRIMARY KEY (QuestionID, SurveyID, ChoiceID, SurveyResponseID),
+	CONSTRAINT FK_QuestionResponseChoices FOREIGN KEY (ChoiceID, QuestionID, SurveyID) REFERENCES Choices(ChoiceID, QuestionID, SurveyID),
 	CONSTRAINT FK_QuestionResponseSurveyResponse FOREIGN KEY (SurveyResponseID) REFERENCES SurveyResponse(SurveyResponseID)
+)
+GO
+CREATE TABLE CreditCards
+(
+	CreditCardID INT IDENTITY(1,1),
+	CreditCardName NVARCHAR(100) NOT NULL,
+	CardImage VARBINARY(MAX) NULL,
+	RedirectLink NVARCHAR(2083) NULL,
+	CONSTRAINT PK_CreditCards PRIMARY KEY (CreditCardID)
 )
 GO
 CREATE TABLE Attributes
@@ -77,15 +86,6 @@ CREATE TABLE ChoiceAttributes
 	CONSTRAINT FK_ChoiceAttributesAttributes FOREIGN KEY (AttributeID) REFERENCES Attributes(AttributeID)
 )
 GO
-CREATE TABLE CreditCards
-(
-	CreditCardID INT IDENTITY(1,1),
-	CreditCardName NVARCHAR(100) NOT NULL,
-	CardImage VARBINARY(MAX) NULL,
-	RedirectLink NVARCHAR(2083) NULL,
-	CONSTRAINT PK_CreditCards PRIMARY KEY (CreditCardID)
-)
-GO
 CREATE TABLE CreditCardAttributes
 (
 	CreditCardID INT NOT NULL,
@@ -96,6 +96,20 @@ CREATE TABLE CreditCardAttributes
 	CONSTRAINT FK_CreditCardAttributesAttributes FOREIGN KEY (AttributeID) REFERENCES Attributes(AttributeID)
 )
 GO
+/*Solution 2 tables*/
+CREATE TABLE CreditCardProperties 
+(
+	CreditCardID INT NOT NULL,
+	Type NVARCHAR(10) NOT NULL,
+	EmploymentStatus NVARCHAR(15) NOT NULL,
+	Features NVARCHAR(15) NOT NULL,
+	Balance NVARCHAR(3) NOT NULL,
+	Discharged NVARCHAR(3) NOT NULL,
+	CONSTRAINT PK_CreditCardProperties PRIMARY KEY (CreditCardID),
+	CONSTRAINT FK_CreditCardPropertiesCreditCards FOREIGN KEY (CreditCardID) REFERENCES CreditCards(CreditCardID)
+)
+GO
+
 
 /*Stored Procedures*/
 /*Pre-Conditions*/
@@ -297,28 +311,98 @@ AS
 	WHERE QuestionID=@QuestionID
 GO
 
-CREATE PROCEDURE ProcessSurvey
-	@MemberID INT,
+/*Process Survey*/
+CREATE PROCEDURE SubmitSurvey 
 	@SurveyID INT,
+	@MemberID INT,
 	@DateSubmitted DATE,
 	@TimeSubmitted TIME
 AS
 	DECLARE @SurveyResponseID INT
 
-	IF (@SurveyID IS NOT NULL AND @DateSubmitted IS NOT NULL AND @TimeSubmitted IS NOT NULL)
-		BEGIN TRANSACTION
-			INSERT INTO SurveyResponse(MemberID, SurveyID, DateSubmitted, TimeSubmitted)
-			VALUES(@MemberID, @SurveyID, @DateSubmitted, @TimeSubmitted)
+	IF(@SurveyID IS NOT NULL AND
+		@DateSubmitted IS NOT NULL AND
+		@TimeSubmitted IS NOT NULL)
+		BEGIN
+			BEGIN TRANSACTION
+			INSERT INTO SurveyResponse(SurveyID, MemberID, DateSubmitted, TimeSubmitted)
+			VALUES(@SurveyID, @MemberID, @DateSubmitted, @TimeSubmitted)
+			
+			SET @SurveyResponseID = SCOPE_IDENTITY()
 
-			SET @SurveyResponseID=SCOPE_IDENTITY()
+			IF @@ERROR<>0
+				ROLLBACK TRANSACTION
+			ELSE
+			BEGIN
+				COMMIT TRANSACTION
+				RETURN @SurveyResponseID
+			END
+		END
+GO
+CREATE PROCEDURE RecordUserResponse 
+	@SurveyResponseID INT,
+	@SurveyID INT,
+	@QuestionID INT, 
+	@ChoiceID INT
+AS
+	IF(@SurveyResponseID IS NOT NULL)
+	BEGIN
+		BEGIN TRANSACTION
+		INSERT INTO QuestionResponse(SurveyID, QuestionID, SurveyResponseID, ChoiceID)
+		VALUES(@SurveyID, @QuestionID, @SurveyResponseID, @ChoiceID)
+
 		IF @@ERROR<>0
 			ROLLBACK TRANSACTION
 		ELSE
-		BEGIN
 			COMMIT TRANSACTION
+	END
+GO
 
+/*Solution 2*/
+CREATE PROCEDURE AddCreditCardProperties
+	@CreditCardID INT,
+	@Type NVARCHAR(10),
+	@EmploymentStatus NVARCHAR(15),
+	@Features NVARCHAR(15),
+	@Balance NVARCHAR(3),
+	@Discharged NVARCHAR(3)
+AS
+	BEGIN TRANSACTION
+	INSERT INTO CreditCardProperties(CreditCardID, Type, EmploymentStatus, Features, Balance, Discharged)
+	VALUES(@CreditCardID, @Type, @EmploymentStatus, @Features, @Balance, @Discharged)
 
-		END
+	IF @@ERROR<>0
+		ROLLBACK TRANSACTION
+	ELSE
+		COMMIT TRANSACTION
+GO
+CREATE PROCEDURE GetUserResponse
+	@SurveyID INT,
+	@QuestionID INT,
+	@ChoiceID INT
+AS
+	SELECT ChoiceText
+	FROM Choices, QuestionResponse
+	WHERE	Choices.QuestionID=QuestionResponse.QuestionID
+		AND	Choices.ChoiceID=QuestionResponse.ChoiceID
+		AND Choices.SurveyID=QuestionResponse.SurveyID
+GO
+CREATE PROCEDURE RecommendCreditCards 
+	@Type NVARCHAR(10),
+	@EmploymentStatus NVARCHAR(15),
+	@Features NVARCHAR(15),
+	@Balance NVARCHAR(3),
+	@Discharged NVARCHAR(3)
+AS
+	SELECT TOP 2	CreditCards.CreditCardName 
+	FROM			CreditCards,
+					CreditCardProperties
+	WHERE CreditCards.CreditCardID=CreditCardProperties.CreditCardID
+			AND CreditCardProperties.Type=@Type
+			AND CreditCardProperties.EmploymentStatus=@EmploymentStatus
+			AND CreditCardProperties.Features=@Features
+			AND CreditCardProperties.Balance=@Balance
+			AND CreditCardProperties.Discharged=@Discharged
 GO
 
 
@@ -330,8 +414,29 @@ EXECUTE AddSurvey 'Credit Card Survey'
 EXECUTE AddQuestion 1, 'What type of card are you looking for?'
 EXECUTE AddChoice 1, 2, 'Student'
 
+EXECUTE AddCreditCard 'TD Classic Travel Visa Card', NULL, 'https://www.tdcanadatrust.com/products-services/banking/credit-cards/view-all-cards/classic-travel.jsp'
+EXECUTE AddCreditCardProperties 1, 'Personal', 'Full Time', 'Travel Rewards', 'No', 'No'
+
+EXECUTE AddCreditCard 'CIBC Aventura Visa Infinite Card', NULL, 'https://www.cibc.com/en/personal-banking/credit-cards/travel-rewards-cards/aventura-visa-infinite.html'
+EXECUTE AddCreditCardProperties 2, 'Personal', 'Full Time', 'Cash Back', 'No', 'No'
+
+EXECUTE AddCreditCard 'CIBC Aventura Visa Card for Business', NULL, 'https://www.cibc.com/ca/small-business/credit-cards/aventura-visa-card-for-bus.html'
+EXECUTE AddCreditCardProperties 3, 'Business', 'Self-employed', 'Travel Rewards', 'No', 'No'
+
+EXECUTE AddCreditCard 'TD Emerald Visa Card', NULL, 'https://www.tdcanadatrust.com/products-services/banking/credit-cards/view-all-cards/emerald-card.jsp'
+EXECUTE AddCreditCardProperties 4, 'Personal', 'Full Time', 'Travel Rewards', 'Yes', 'No'
+
+EXECUTE AddCreditCard 'TD Cash Back MasterCard Card', NULL, 'https://www.tdcanadatrust.com/products-services/banking/credit-cards/view-all-cards/cashback-master-card.jsp'
+EXECUTE AddCreditCardProperties 5, 'Personal', 'Full Time', 'Cash Back', 'No', 'No'
+
 EXECUTE LoadQuestions 1
 EXECUTE LoadChoices 2
+
+EXECUTE SubmitSurvey 1, NULL, '4/12/2017', '11:24 AM', 1, 1 /*SurveyID, MemberID, DateSubmitted, TimeSubmitted, QuestionID, ChoiceID*/
+EXECUTE RecordUserResponse 1, 1, 2, 3 /*SurveyResponseID, SurveyID, QuestionID, ChoiceID*/
+
+EXECUTE GetUserResponse 1, 1, 1 /*SurveyID, QuestionID, ChoiceID*/
+EXECUTE RecommendCreditCards 'Personal', 'Full Time', 'Cash Back', 'No', 'No' /*Type, EmploymentStatus, Features, Balance, Discharged*/
 
 SELECT*FROM Choices
 SELECT*FROM Questions
@@ -340,6 +445,11 @@ SELECT*FROM Members
 SELECT*FROM CreditCards
 SELECT*FROM Attributes
 SELECT*FROM CreditCards
+SELECT*FROM SurveyResponse
+SELECT*FROM QuestionResponse
+SELECT*FROM CreditCardProperties
+
+/*WEIGHTED VALUES*/
 SELECT*FROM CreditCardAttributes
 SELECT*FROM ChoiceAttributes
 
